17b844df948fc8514fcf85ea820a2c7b66384196
Pandora De Canopus <pandoradecanopus@gmail.com>
2026-02-19 20:13:03 -0400

    Aula03_Java_processoDados

---
+++ aula02_processoDados/src/Main.java
@@ -0,0 +1,29 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// Primeiro Processo
+		int x, y;
+		x = 5;
+		y = 2*x;
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		// Segundo Processo
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b;
+		
+		System.out.println(resultado);
+		
+	}
+
+}
